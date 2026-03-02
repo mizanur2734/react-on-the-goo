e410e146f9b9785746a4c53518c0a57cc94e0d6a
mizanur2734 <mizanurbb2791@gmail.com>
2026-03-02 22:21:36 +0600

    added the country

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-
-import './App.css'
-import Countries from './countries/Countries/Countries'
+import { Suspense } from "react";
+import "./App.css";
+import Countries from "./countries/Countries/Countries";
 
 function App() {
- 
-
+  const countriesPromise = fetch(
+    "https://openapi.programming-hero.com/api/all",
+  ).then((res) => res.json());
   return (
     <>
-     
-      <Countries></Countries>
-     
+      <Suspense fallback={<h3>Nadir loading...</h3>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
